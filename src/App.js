73f77bcc3d8bd472a95ec0a4d4ac47a870baf277
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-07-23 08:50:08 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Tetris from './components/Tetris';
+import { inject } from '@vercel/analytics';
 
 const App = () => (
   <div className="App">
     <Tetris />
   </div>
 );
+
+inject();
 
 export default App;
